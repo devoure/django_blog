10fc8c18271acfadcdd93d185cfcbc1f27fb6c85
Athman Bakari <disguisedsandwich@gmail.com>
2020-11-01 17:13:28 +0300

    Update 1.8

--- django_test/settings.py
+++ django_test/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
 """
 from pathlib import Path
-from .local_settings import *
 import os
 #import psycopg2
 import dj_database_url
